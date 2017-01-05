e180ba268312d2af6370b8274026b5e12cb2262d
Bonstor <glebvolohov@gmail.com>
2017-01-05 17:03:43 +0300

    Update

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+import main
 token = '299872977:AAEC9oojPArwtduUaqKGLPFWx-2WVspOpdw'
+start_list = ['Bonstor']
+
+
+def init():
+    main.petyh_list = start_list
--- main.py
+++ main.py
@@ -14,6 +14,26 @@ def new_answer(message):
     answer_list.append(message.text)
     bot.reply_to(message, 'Добавлено')
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,'Привет привет, мой юный друг.\n'
+                                     'Я сумрочное творение 17й кафедры призванное ставить петухов на место\n'
+                                     'Вот простое описание моего незамысловатого функционала:\n'
+                                     'Эту команду могут использовать все,'
+                                     ' добавлаяя свои остроумные осткарбления петушар\n'
+                                     '/добавь Сообщение для петуха\n'
+                                     'Этим сообщением вы можете добавить петуха в список петушения\n'
+                                     '/петух Ник петуха\n'
+                                     'Чтобы удалить всех из листа петушения\n'
+                                     '/отпусти\n'
+                                     'Что бы связаться с больным ублюдком, написавшим это '
+                                     'бессмысленное приложение просто напиши\n'
+                                     '/ублюдок Сообщение для больного ублюдка который придумал это говно\n'
+                                     'Ps Тимошка выражает искренную благодарность '
+                                     'Юлечке Лавдиной за помощь в разработке приложения'
+                                     )
+
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
@@ -49,9 +69,29 @@ def repeat_all_messages(message):
         return 0
 
     if message.text.startswith('/петух '):
-        bot.send_message(message.chat.id, "Ах ты маленький засранец!!!\n" + message.from_user.first_name + " вздумал наебать меня?!?!?!")
-        bot.send_message(message.chat.id, "Выслеживаю нового петушару!\n" + message.from_user.first_name + " привет привет!")
+        bot.send_message(message.chat.id,
+                         "Ах ты маленький засранец!!!\n" + message.from_user.first_name + " вздумал наебать меня?!?!?!")
+        bot.send_message(message.chat.id,
+                         "Выслеживаю нового петушару!\n" + message.from_user.first_name + " привет привет!")
         petyh_list.append(message.from_user.id)
+        return 0
+
+    if message.text.startswith('/отпусти ') and (message.from_user.id == 234672324):
+        bot.send_message(message.chat.id, "Все свободны. Ну кроме главного петушары конечно")
+        config.init()
+        return 0
+
+    if message.text.startswith('/отпусти '):
+        bot.send_message(message.chat.id,
+                         "Ах ты маленький засранец!!!\n" + message.from_user.first_name + " вздумал наебать меня?!?!?!")
+        bot.send_message(message.chat.id,
+                         "Выслеживаю нового петушару!\n" + message.from_user.first_name + " привет привет!")
+        petyh_list.append(message.from_user.id)
+        return 0
+
+    if message.text.startswith('/уб'):
+        bot.send_message(254247271,message.from_user)
+        bot.send_message(254247271,message.text)
         return 0
 
 
